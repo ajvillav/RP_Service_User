dc5126b229be40a35e86e145961a012aa912eb72
ajvillav <ajvillav@unal.edu.co>
2021-08-18 23:25:27 -0500

    Create RP_Service_User

---
+++ src/main/java/com/recognitionprogram/userservice/RpuserserviceApplication.java
@@ -0,0 +1,13 @@
+package com.recognitionprogram.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpuserserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpuserserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/recognitionprogram/userservice/RpuserserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recognitionprogram.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpuserserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
